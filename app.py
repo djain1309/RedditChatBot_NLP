24b3d903cdf266cf2c2df917ad922f1deeb57c92
Tanmay <tanmayag78@gmail.com>
2022-12-06 20:42:35 -0500

    Changes log

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+import pandas as pd
+from flask import Flask, make_response
 from flask import request
 from flask_cors import CORS
 from chatbot import query_chatbot
@@ -19,6 +20,16 @@ def execute():
         'response': response
     }
 
+
+
+@app.route("/logs",methods=['GET'])
+def get_logs():
+
+    df = pd.read_csv("conversation.csv")
+    resp = make_response(df.to_csv())
+    resp.headers["Content-Disposition"] = "attachment; filename=query_logs.csv"
+    resp.headers["Content-Type"] = "text/csv"
+    return resp
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=9999)
